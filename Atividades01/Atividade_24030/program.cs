1c88d44f9805e702e3a5ddb53aa364c07ba53b64
improvez <161395547+improvez@users.noreply.github.com>
2024-03-11 18:40:35 -0300

    Update and rename readme.md to program.cs

---
+++ Atividades01/Atividade_24030/program.cs
@@ -0,0 +1,29 @@
+
+// para criar outro projeto.
+//dotnet new console -n "Atividade_240304"
+
+//1) após sincronizada a sua branch main, tire uma nova branch para o exercicio
+//2) crie um novo projeto na pasta atividades com o nome Atividade_240304
+//3) crie um programa em c# que seja capaz de realizar as 4 operações aritmeticas básicas.
+//a) leia o primeiro operando;
+//b) leia o operador aritmetico;
+//c) leia o segundo numero;
+//Identifique qual operação foi solicitado pelo usuário,
+//permitindo trabalhar com numeros reais e inteiros, processe e exiba o resultado.
+
+//Ao final, envie a branch para o github e abra um pull request.
+
+switch (calculadora)
+        {
+            case '+':
+                Console.WriteLine($"{num1 + num2}");
+                break;
+            case '-':
+                Console.WriteLine($"{num1 - num2}");
+                break;
+            case '*':
+                Console.WriteLine($"{num1 * num2}");
+                break;
+            case '/':
+                Console.WriteLine($"{num1 / num2}");
+        }
